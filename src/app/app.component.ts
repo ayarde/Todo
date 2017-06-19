b595e0887307b4fb642c7d44c01a3bbe28662264
ayarde <adrian.ayarde@gmail.com>
2017-06-19 17:24:58 -0300

    Creating separate components to display a list of todo's and a single todo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import {Todo} from './todo';
 import {TodoDataService} from './todo-data.service';
 
@@ -6,30 +6,36 @@ import {TodoDataService} from './todo-data.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
+  providers: []
 })
 export class AppComponent {
 
-  newTodo: Todo = new Todo();
-
+  // No longer needed, now handled by TodoListHeaderComponent
+  // newTodo: Todo = new Todo();
   constructor(private todoDataService: TodoDataService) {
   }
+  // No longer needed, now handled by TodoListHeaderComponent
+  // addTodo() {
+  //   this.todoDataService.addTodo(this.newTodo);
+  //   this.newTodo = new Todo();
+  // }
 
-  addTodo() {
-    this.todoDataService.addTodo(this.newTodo);
-    this.newTodo = new Todo();
+  // Add new method to handle event emitted by TodoListHeaderComponent
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
   }
 
-  toggleTodoComplete(todo) {
+  // rename from toggleTodoComplete
+  onToggleTodoComplete(todo: Todo) {
     this.todoDataService.toggleTodoComplete(todo);
   }
 
-  removeTodo(todo) {
+  // rename from removeTodo
+  onRemoveTodo(todo: Todo) {
     this.todoDataService.deleteTodoById(todo.id);
   }
 
   get todos() {
     return this.todoDataService.getAllTodos();
   }
-
 }
