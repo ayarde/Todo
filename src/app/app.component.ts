22c2ec89e3d88b1120d851629c3d0e6358bd6451
ayarde <adrian.ayarde@gmail.com>
2017-06-20 22:40:41 -0300

    Create an API Service to Talk to a REST Backend.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,60 @@
-import { Component } from '@angular/core';
-import {Todo} from './todo';
+import { Component, OnInit } from '@angular/core';
 import {TodoDataService} from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: []
+  providers: [TodoDataService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  // No longer needed, now handled by TodoListHeaderComponent
-  // newTodo: Todo = new Todo();
-  constructor(private todoDataService: TodoDataService) {
-  }
-  // No longer needed, now handled by TodoListHeaderComponent
-  // addTodo() {
-  //   this.todoDataService.addTodo(this.newTodo);
-  //   this.newTodo = new Todo();
-  // }
+  todos: Todo[] = [];
 
-  // Add new method to handle event emitted by TodoListHeaderComponent
-  onAddTodo(todo: Todo) {
-    this.todoDataService.addTodo(todo);
+  constructor(
+    private todoDataService: TodoDataService
+  ) {
   }
 
-  // rename from toggleTodoComplete
-  onToggleTodoComplete(todo: Todo) {
-    this.todoDataService.toggleTodoComplete(todo);
+  public ngOnInit() {
+    this.todoDataService
+      .getAllTodos()
+      .subscribe(
+        (todos) => {
+          this.todos = todos;
+        }
+      );
   }
 
-  // rename from removeTodo
-  onRemoveTodo(todo: Todo) {
-    this.todoDataService.deleteTodoById(todo.id);
+  onAddTodo(todo) {
+    this.todoDataService
+      .addTodo(todo)
+      .subscribe(
+        (newTodo) => {
+          this.todos = this.todos.concat(newTodo);
+        }
+      );
   }
 
-  get todos() {
-    return this.todoDataService.getAllTodos();
+  onToggleTodoComplete(todo) {
+    this.todoDataService
+      .toggleTodoComplete(todo)
+      .subscribe(
+        (updatedTodo) => {
+          todo = updatedTodo;
+        }
+      );
   }
+
+  onRemoveTodo(todo) {
+    this.todoDataService
+      .deleteTodoById(todo.id)
+      .subscribe(
+        (_) => {
+          this.todos = this.todos.filter((t) => t.id !== todo.id);
+        }
+      );
+  }
+
 }
